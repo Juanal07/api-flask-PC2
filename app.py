08ac83327aa7ade1be7f910f1e1bbec3a8b7170f
Juan Alberto Raya <jjrr1307@gmail.com>
2021-03-22 14:57:09 +0100

    scraping

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ from flask import Flask, jsonify, abort, request, make_response
 
 from flask_cors import CORS
 
-from src.sentiment import getSentiment
+from src.webscraping import ws_15mpedia
 
-app = Flask(__name__)
-CORS(app)
+def main():
+    app = Flask(__name__)
+    CORS(app)
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error':'Not found'}),404)
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response(jsonify({'error':'Not found'}),404)
 
-@app.route('/sentiment', methods = ['POST'])
-def sentiment():
-    return getSentiment(request.json['text'])
+    @app.route('/sentiment', methods = ['POST'])
+    def sentiment():
+        return jsonify({'result': 0.7})
+
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # main()
+    ws_15mpedia.scraping()
