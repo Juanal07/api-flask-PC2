67d5a0aa14ec0c0480c78179a44cd26167a17a4b
Juan Alberto Raya <jjrr1307@gmail.com>
2021-04-06 23:35:02 +0200

    auto push

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, abort, request, make_response
+from flask_cors import CORS
+from src.webscraping import ws_15mpedia
+from src.webscraping import ws_opiniones
+
+def main():
+    app = Flask(__name__)
+    CORS(app)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return make_response(jsonify({'error':'Not found'}),404)
+
+    @app.route('/sentiment', methods = ['POST'])
+    def sentiment():
+        return jsonify({'result': 0.7})
+
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    # main()
+    # ws_15mpedia.scraping()
+    ws_opiniones.scraping()
