2de724b489084c2656e1eccab850eb803b4adfb1
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-04-25 17:24:57 +0200

    finalizo insertar municipios

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.webscraping import ws_15mpedia
 from src.webscraping import ws_opiniones
 from src.webscraping import ws_supermercados
 from src.webscraping import ws_noticias
+from src.webscraping import ws_estaciones
 from src.sentiment import sentiment
 
 def main():
@@ -51,4 +52,5 @@ def webscraping():
 if __name__ == '__main__':
     # main()
     # webscraping()
-    ws_15mpedia.scraping()
+    # ws_15mpedia.scraping()
+    ws_estaciones.scrap()
