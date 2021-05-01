8cc991401b348f6a7c733c4ab726f55ef672ff7c
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-01 11:25:11 +0200

    select de id municipio

--- src/webscraping/ws_estaciones.py
+++ src/webscraping/ws_estaciones.py
@@ -1,6 +1,25 @@
 import csv
+import mariadb
+import sys
 
 def scrap():
+    #Conexión con BBDD
+    try:
+        conn = mariadb.connect(
+            user="pr_softlusion",
+            password="Softlusion",
+            host="2.139.176.212",
+            port=3306,
+            database="prsoftlusion"
+        )
+        print("Conexión a BBDD")
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+    
     with open('listado-estaciones-completo-sel.csv', newline='', encoding='utf-8') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=';')
         for row in spamreader:
@@ -8,7 +27,7 @@ def scrap():
             # codeInt = int(code) 
             name = row[1]
             dir = row[2]
-            city = row[3]
+            city:str = row[3]
             cercanias = row[4]
             if cercanias=="SI":
                 cercaniasInt = 1
@@ -20,9 +39,18 @@ def scrap():
             else:
                 feveInt = 1
             print(code,", ",name,", ",name,", ",dir,", ",city,", ",cercaniasInt,", ",feveInt)
+            idMunicipality = 0
+            # query = ("SELECT idMunicipality FROM municipality WHERE name= '%s'")
+            # tupla:tuple = (city)
+            # cur.execute(query,tupla)
+            # cur.execute("SELECT idMunicipality FROM municipality WHERE name=?", (city))
+            query = "SELECT idMunicipality FROM municipality WHERE name= '"+ city+ "'"
+            cur.execute(query)
+            for (idMunicipality) in cur:
+                print(idMunicipality)
             # print(', '.join(row))
 
 
-
-    
+    # conn.commit()
+    conn.close()
     return None
