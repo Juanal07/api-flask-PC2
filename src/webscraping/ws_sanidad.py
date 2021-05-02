c99d892cab534574d22c95777b2d50882c1c615a
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-02 23:04:42 +0200

    modificacion tras formateo de csv

--- src/webscraping/ws_sanidad.py
+++ src/webscraping/ws_sanidad.py
@@ -2,8 +2,8 @@ import csv
 
 def scrap():
 
-    with open('csv\20210502_Centros_de_Atencion_Primaria.csv', newline='', encoding='utf-8') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
+    with open('csv\/20210502_Centros_de_Atencion_Primaria.csv', newline='', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
         for row in spamreader:
             # print(', '.join(row))
             for i in row:
