d48ba4af97ff4616b98c613ca74554779b2a2560
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-05 14:45:36 +0200

    completo el resto de inserciones

--- src/webscraping/ws_sanidad.py
+++ src/webscraping/ws_sanidad.py
@@ -40,15 +40,62 @@ def scrap():
                     idMunicipio = idMunicipality[0]
                     print(idMunicipio)
                 try:
-                    cur.execute("INSERT INTO medicalcenter_copy(name,type,address,idMunicipality) VALUES"+
+                    cur.execute("INSERT INTO medicalcenter(name,type,address,idMunicipality) VALUES"+
                     "(?,?,?,?)",(name,tipo,address,idMunicipality[0]))
                 except:
                     errores +=1
                     print("ERROR")
             cont += 1
 
+    #Hospitales
+    cont = 0
+    with open('csv\/20210502_Catalogo_de_Hospitales.csv', newline='', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            # print(', '.join(row))
+            if cont != 0:
+                name = row[2]
+                municipio = row[5]
+                address = row[6]
+                tipo = row[10]
+                print(name," ",municipio," ",address," ",tipo)
+                idMunicipality = 0
+                cur.execute("SELECT idMunicipality FROM municipality WHERE name=?", (municipio,))
+                for (idMunicipality) in cur:
+                    idMunicipio = idMunicipality[0]
+                    print(idMunicipio)
+                try:
+                    cur.execute("INSERT INTO medicalcenter(name,type,address,idMunicipality) VALUES"+
+                    "(?,?,?,?)",(name,tipo,address,idMunicipality[0]))
+                except:
+                    errores +=1
+                    print("ERROR")
+            cont += 1
 
-
+    #Centros de Urgencias
+    cont = 0
+    with open('csv\/20210502_Dispositivos_de_Urgencia_Extrahospitalaria.csv', newline='', encoding='utf-8') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';')
+        for row in spamreader:
+            # print(', '.join(row))
+            if cont != 0:
+                name = row[3]
+                municipio = row[1]
+                address = row[4]
+                tipo = "Dispositivos de Urgencia Extrahospitalaria"
+                print(name," ",municipio," ",address," ",tipo)
+                idMunicipality = 0
+                cur.execute("SELECT idMunicipality FROM municipality WHERE name=?", (municipio,))
+                for (idMunicipality) in cur:
+                    idMunicipio = idMunicipality[0]
+                    print(idMunicipio)
+                try:
+                    cur.execute("INSERT INTO medicalcenter(name,type,address,idMunicipality) VALUES"+
+                    "(?,?,?,?)",(name,tipo,address,idMunicipality[0]))
+                except:
+                    errores +=1
+                    print("ERROR")
+            cont += 1
 
     print("Errores: ",errores)
     conn.commit()
