9fd93dd053ec49764dfe5d45815ad273748a481f
Javier <60470734+javiaspiroz@users.noreply.github.com>
2021-05-19 22:30:33 +0200

    exporto jupyter a python script

--- src/webscraping/mineria.py
+++ src/webscraping/mineria.py
@@ -10,17 +10,19 @@ from nltk import word_tokenize, download
 from nltk.stem import SnowballStemmer
 download('punkt')
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-import joblib #para exportar el modelo
+import pickle #para exportar el modelo
 
+# %%
 # Creamos el array de textos y el array de categorias
 x=[]
 y=[]
 
+# %%
 # Cargamos el dataset
 for cat in os.listdir('../newspaper-articles-main/'):
     for txt in os.listdir('../newspaper-articles-main/'+cat+'/'):
@@ -28,10 +30,14 @@ for cat in os.listdir('../newspaper-articles-main/'):
             x.append(f.read())
             y.append(cat)
 
+# %%
 # Mostramos el array de textos
-print(x)
-print(y)
+x
 
+# %%
+y
+
+# %%
 # Pasamos el array de categorias a valores numericos
 y_num=[]
 value_des = 0
@@ -44,14 +50,19 @@ for i in y:
         y_num.append(1)
         value_nodes+=1
 
-# Mostramos el array de categorias por numero
-print(y_num)
 
+# %%
+# Mostramos el array de categorias por numero
+y_num
+
+# %%
 # Mostramos numero de textos por categoria
 names = ['Despoblacion', 'No despoblacion']
 values = [value_des, value_nodes]
 plt.bar(names, values)
 
+
+# %%
 # Proceso de limpieza del array de textos
 spanish_stemmer = SnowballStemmer('spanish')
 corpus = []
@@ -65,21 +76,42 @@ for texto in x:
     stemmed = ' '.join(tokenized)
     corpus.append(stemmed)
 
+
+# %%
 # Cargamos el texto de stop words en castellano, lo mostramos
 with open('../stopword.txt', 'r') as file:
     my_stopwords=[file.read().replace('\n', ',')]
 print(my_stopwords)
 
+# %%
 # Creamos Matrix TF-IDF aplicando nuestras stop words y la mostramos
 cv_tfidf = TfidfVectorizer(analyzer='word', stop_words = my_stopwords)
 X_tfidf = cv_tfidf.fit_transform(corpus).toarray()
 pd.DataFrame(X_tfidf, columns=cv_tfidf.get_feature_names())
 
-# Dividimos nuestros datos en train/test al 80/20 y mostramos que categorias hay en cada grupo
+#Guardamos el TF-IDF
+# vectorizer = CountVectorizer(decode_error="replace")
+# vec_train = vectorizer.fit_transform(corpus)
+# Fit the model
+# tf_transformer = cv_tfidf.fit(corpus)
+
+# Dump the file
+# pickle.dump(tf_transformer, open("tfidf1.pkl", "wb"))
+#Save vectorizer.vocabulary_
+# pickle.dump(vectorizer.vocabulary_,open("feature.pkl","wb"))
+
+# Guardar diccionario
+with open("wordList2.pk", 'wb') as file:
+    pickle.dump(cv_tfidf, file)
+
+# %%
+# Dividimos nuestros datos en train/test al 80/20
+# y mostramos que categorias hay en cada grupo
 X_train, X_test, y_train, y_test = train_test_split(X_tfidf, y_num, test_size=0.2, random_state=0)
 print(y_train)
 print(y_test)
 
+# %%
 # Para los modelos que queremos probar: entrenamos, testeamos, mostramos la accuracy,
 # la classification reoprt y la confusion matrix
 model = DecisionTreeClassifier(random_state=6)
@@ -93,7 +125,11 @@ gd_sr = GridSearchCV(estimator=model,
                      cv=3,
                      n_jobs=-1)
 gd_sr.fit(X_train, y_train)
-joblib.dump(gd_sr, 'modelo.pkl') #Guardo el modelo
+# pickle.dump(gd_sr, open("modelo.pickle", "wb")) #Guardo el modelo
+
+with open("modeloNoticias.pk", 'wb') as file:
+        pickle.dump(gd_sr, file)
+
 best_parameters = gd_sr.best_params_
 print(best_parameters)
 best_result = gd_sr.best_score_
@@ -104,13 +140,16 @@ print("Accuracy test: " + str(acc) + '\n')
 print(classification_report(y_test, prediction))
 print(confusion_matrix(y_test, prediction))
 
+# %%
 # Mostramos la confusion matrix con mapa de calor
+
 conf_mat = confusion_matrix(y_test, prediction)
 sns.heatmap(conf_mat, annot=True, fmt='d',
             xticklabels=['despoblacion','no despoblacion'], yticklabels=['despoblacion','no despoblacion'])
 plt.ylabel('Actual')
 plt.xlabel('Predicted')
 
+# %%
 # Clasificamos textos al momento
 texts = ["Los pueblos de la españa vaciada",
          "El capitan tsubasa marca un gol para el equipo de japon",
